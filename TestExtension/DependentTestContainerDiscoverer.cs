1158dc1d2b27ae0cf1e5498a7ae599150b423d0c
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-08-22 20:58:57 +0900

    クラス追加 ・DependentTestDiscoverer ・DependentTestContainerDiscoverer

---
+++ TestExtension/DependentTestContainerDiscoverer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TestWindow.Extensibility;
+
+namespace TestExtension
+{
+	[Export(typeof(ITestContainerDiscoverer))]
+	public class DependentTestContainerDiscoverer : ITestContainerDiscoverer
+	{
+		internal const string UriString = "executor://DependentTestExecutor/v1";
+
+		public event EventHandler TestContainersUpdated;
+		private readonly IServiceProvider serviceProvider;
+		private readonly ILogger logger;
+
+		public Uri ExecutorUri { get; } = new Uri(UriString);
+		public IEnumerable<ITestContainer> TestContainers { get; } = Enumerable.Empty<ITestContainer>();
+
+		[ImportingConstructor]
+		public DependentTestContainerDiscoverer(
+			[Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider,
+			ILogger logger)
+		{
+			this.serviceProvider = serviceProvider;
+			this.logger = logger;
+		}
+	}
+}
---
+++ TestExtension/DependentTestDiscoverer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace TestExtension
+{
+	[DefaultExecutorUri(DependentTestContainerDiscoverer.UriString)]
+	[FileExtension(".xml")]
+	public class DependentTestDiscoverer : ITestDiscoverer
+	{
+		public void DiscoverTests(
+			IEnumerable<string> sources,
+			IDiscoveryContext discoveryContext,
+			IMessageLogger logger,
+			ITestCaseDiscoverySink discoverySink)
+		{
+
+		}
+	}
+}

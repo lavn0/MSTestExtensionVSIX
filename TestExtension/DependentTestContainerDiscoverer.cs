fcc6ba906345a94e1fcd7eee0a6c0a35d3b5eccc
LAVN <rairairakurakulilac@hotmail.co.jp>
2016-09-10 15:31:52 +0900

    プロジェクトでビルド時に出力されるファイルのパスを取得できるよう修正

--- TestExtension/DependentTestContainerDiscoverer.cs
+++ TestExtension/DependentTestContainerDiscoverer.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Diagnostics;
 using System.Linq;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TestWindow;
+using Microsoft.VisualStudio.TestWindow.Controller;
 using Microsoft.VisualStudio.TestWindow.Extensibility;
 
 namespace TestExtension
@@ -15,17 +18,33 @@ namespace TestExtension
 
 		public event EventHandler TestContainersUpdated;
 		private readonly IServiceProvider serviceProvider;
+		private readonly SafeDispatcher safeDispatcher;
 		private readonly ILogger logger;
 
 		public Uri ExecutorUri { get; } = new Uri(UriString);
-		public IEnumerable<ITestContainer> TestContainers { get; } = Enumerable.Empty<ITestContainer>();
+		public IEnumerable<ITestContainer> TestContainers
+		{
+			get
+			{
+				var projects = ((IVsSolution)this.serviceProvider.GetService(typeof(SVsSolution))).EnumerateLoadedProjects().ToList();
+				var outputPaths = projects.Select(p => VSProjectExtensions.GetProjectOutputPath(p, this.serviceProvider, this.safeDispatcher, logger)).ToList();
+				foreach (var outputPath in outputPaths)
+				{
+					Debug.WriteLine($"Output File Found. Path= '{outputPath}'");
+				}
+
+				return Enumerable.Empty<ITestContainer>();
+			}
+		}
 
 		[ImportingConstructor]
 		public DependentTestContainerDiscoverer(
 			[Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider,
+			SafeDispatcher safeDispatcher,
 			ILogger logger)
 		{
 			this.serviceProvider = serviceProvider;
+			this.safeDispatcher = safeDispatcher;
 			this.logger = logger;
 
 			var sbm = ServiceProvider.GlobalProvider.GetService(typeof(SVsSolutionBuildManager)) as IVsSolutionBuildManager2;
